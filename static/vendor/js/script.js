b912050c2d48490468773dfe121c02deab468de8
eronryabets <eronryabets@gmail.com>
2024-05-03 16:34:04 +0300

    food page (modal window call - updated)

--- static/vendor/js/script.js
+++ static/vendor/js/script.js
@@ -274,35 +274,4 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 }
 
-    // function postData(form) {
-    //     form.addEventListener('submit', (e) => {
-    //         e.preventDefault();
-    //
-    //         const statusMessage = document.createElement('div');
-    //         statusMessage.classList.add('status');
-    //         statusMessage.textContent = message.loading;
-    //         form.append(statusMessage);
-    //
-    //         const request = new XMLHttpRequest();
-    //         request.open('POST', 'http://localhost:8000/food/');
-    //
-    //         request.setRequestHeader('Content-type', 'multipart/form-data');
-    //         const formData = new FormData(form);
-    //
-    //         request.send(formData);
-    //
-    //         request.addEventListener('load', () => {
-    //             if (request.status === 200) {
-    //                 console.log(request.response);
-    //                 statusMessage.textContent = message.success;
-    //             } else {
-    //                 statusMessage.textContent = message.failure;
-    //             }
-    //         });
-    //
-    //
-    //     });
-    // }
-
-
 });

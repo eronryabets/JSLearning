98ef304fde9d5825c5990066b7c8a2a9de732099
eronryabets <eronryabets@gmail.com>
2024-05-09 12:56:09 +0300

    axios

--- static/vendor/js/script.js
+++ static/vendor/js/script.js
@@ -191,19 +191,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-     const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if(!res.ok) {
-           throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    };
-
-    getResource('http://localhost:8000/api/menu/')
+    axios.get('http://localhost:8000/api/menu/')
         .then(data => {
-           data.forEach(({img, altimg, title, descr, price} )=> {
+            data.data.forEach(({img, altimg, title, descr, price} )=> {
                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
            })
         });

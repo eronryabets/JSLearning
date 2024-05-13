9e77ff654d2bf0212d86e422524d2949500af1ba
eronryabets <eronryabets@gmail.com>
2024-05-13 17:01:23 +0300

    Webpack - (refactoring - modules)

---
+++ static/vendor/js/modules/forms.js
@@ -0,0 +1,90 @@
+function forms() {
+    const forms = document.querySelectorAll('form');
+
+    const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+
+    const message = {
+        loading: '/media/img/form/spinner.svg',
+        success: 'Thank you, we will contact you soon!',
+        failure: 'Something go wrong...'
+    };
+
+    forms.forEach(item => {
+        bindPostData(item);
+    });
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrfToken,
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            postData('http://localhost:8000/api/requests/', json)
+                .then(data => {
+                    console.log('Data:', JSON.stringify(data, null, 2));
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+
+        });
+    }
+
+    //post form
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
+
+    fetch('http://localhost:8000/api/menu/') // /requests
+        .then(data => data.json())
+        .then(res => console.log(res))
+}
+
+module.exports = forms;
